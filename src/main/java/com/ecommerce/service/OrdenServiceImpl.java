8464417c6267e203b09c40fbb94123451703bec4
baruch hdz <baruchhdz@baruchs-iMac.home>
2023-01-11 00:31:43 -0500

    creacion de repositorios y servicios para la orden y el detalle orden

---
+++ src/main/java/com/ecommerce/service/OrdenServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ecommerce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.model.Orden;
+import com.ecommerce.repository.IOrdenRepository;
+
+@Service
+public class OrdenServiceImpl implements IOrdenService{
+
+	@Autowired //Inyectar objeto a clase
+	private IOrdenRepository ordenRepository;
+	
+	@Override
+	public Orden save(Orden orden) {
+		return ordenRepository.save(orden);
+	}
+}
